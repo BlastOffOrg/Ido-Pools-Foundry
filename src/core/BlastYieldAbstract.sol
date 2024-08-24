8c72e1f4e886b1a92e00b4fffd294d8d9a117f21
0xheartcode <0xheartcode@gmail.com>
2024-08-24 17:55:20 +0200

    Bug Fix: BlastYield bug fixes && natspec update

--- src/core/BlastYieldAbstract.sol
+++ src/core/BlastYieldAbstract.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.20;
 
 import "openzeppelin-contracts-upgradeable/contracts/access/Ownable2StepUpgradeable.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 interface IBlast {
     function configureClaimableYield() external;
     function configureClaimableGas() external;
     function claimMaxGas(address contractAddress, address recipient) external returns (uint256);
     function claimAllGas(address contractAddress, address recipient) external returns (uint256);
-    function claimYield(address contractAddress, address recipient) external returns (uint256);
+    function claimAllYield(address contractAddress, address recipient) external returns (uint256);
     function readClaimableYield(address contractAddress) external view returns (uint256);
 }
 
@@ -28,7 +29,7 @@ contract BlastYieldAbstract is Ownable2StepUpgradeable {
     IBlast private constant BLAST = IBlast(0x4300000000000000000000000000000000000002);
     IERC20Rebasing private constant USDB = IERC20Rebasing(0x4300000000000000000000000000000000000003);
     IERC20Rebasing private constant WETH = IERC20Rebasing(0x4300000000000000000000000000000000000004);
-    
+
     // NOTE: the commented lines below are the testnet addresses
     // IERC20Rebasing private constant USDB = IERC20Rebasing(0x4200000000000000000000000000000000000022);
     // IERC20Rebasing private constant WETH = IERC20Rebasing(0x4200000000000000000000000000000000000023);
@@ -42,12 +43,12 @@ contract BlastYieldAbstract is Ownable2StepUpgradeable {
     event ETHYieldClaimed(uint256 amount);
 
     error InsufficientETHYield(uint256 requested, uint256 available);
-    error ETHTransferFailed();
     error InvalidToken(address token);
     error InsufficientWETHYield();
     error InsufficientUSDBYield();
+    error TransferFailed();
 
-    function __BlastYieldAbstract_init() internal initializer {
+    function __BlastYieldAbstract_init() internal onlyInitializing {
         __Ownable2Step_init();
         WETH.configure(YieldMode.CLAIMABLE);
         USDB.configure(YieldMode.CLAIMABLE);
@@ -56,6 +57,14 @@ contract BlastYieldAbstract is Ownable2StepUpgradeable {
 
     }
 
+    /**
+        * @notice Claims all available yield for WETH, USDB, and ETH
+        * @dev This function can only be called by the contract owner
+        * @dev Claims WETH and USDB yield using the IERC20Rebasing interface
+        * @dev Claims ETH yield using the IBlast interface
+        * @dev Updates the accumulated yield amounts for each token type
+        * @dev Emits YieldClaimed events for WETH and USDB, and ETHYieldClaimed for ETH
+        */
     function claimYield() public onlyOwner {
         address smartContract = address(this);
 
@@ -75,12 +84,23 @@ contract BlastYieldAbstract is Ownable2StepUpgradeable {
 
         uint256 ethClaimableAmount = BLAST.readClaimableYield(smartContract);
         if (ethClaimableAmount > 0) {
-            uint256 claimed = BLAST.claimYield(smartContract, smartContract);
+            uint256 claimed = BLAST.claimAllYield(smartContract, smartContract);
             accumulatedETHYield += claimed;
             emit ETHYieldClaimed(claimed);
         }
     }
 
+    /**
+        * @notice Withdraws accumulated yield for WETH or USDB
+        * @dev This function can only be called by the contract owner
+        * @param token The address of the token to withdraw (must be WETH or USDB)
+        * @param recipient The address to receive the withdrawn yield
+        * @param amount The amount of yield to withdraw
+        * @custom:throws InvalidToken if the token is not WETH or USDB
+        * @custom:throws InsufficientWETHYield if trying to withdraw more WETH than accumulated
+        * @custom:throws InsufficientUSDBYield if trying to withdraw more USDB than accumulated
+        * @custom:throws TransferFailed if the token transfer fails
+        */
     function withdrawAccumulatedYield(address token, address recipient, uint256 amount) public onlyOwner {
         if (token != address(WETH) && token != address(USDB)) {
             revert InvalidToken(token);
@@ -97,9 +117,21 @@ contract BlastYieldAbstract is Ownable2StepUpgradeable {
             }
             accumulatedUSDBYield -= amount;
         }        
-        IERC20Rebasing(token).claim(recipient, amount);
+
+        bool success = IERC20(token).transfer(recipient, amount);
+        if (!success) {
+            revert TransferFailed();
+        }
+
     }
 
+    /**
+        * @notice Claims all available gas yield
+        * @dev This function can only be called by the contract owner
+        * @dev Uses the IBlast interface to claim all gas
+        * @dev Adds the claimed amount to the accumulated ETH yield
+        * @dev Emits a GasClaimed event with the claimed amount
+        */
     function claimGas() public onlyOwner {
         address smartContract = address(this);
         uint256 claimed = BLAST.claimAllGas(smartContract, smartContract);
@@ -107,6 +139,13 @@ contract BlastYieldAbstract is Ownable2StepUpgradeable {
         emit GasClaimed(claimed);
     }
 
+    /**
+        * @notice Claims the maximum available gas yield
+        * @dev This function can only be called by the contract owner
+        * @dev Uses the IBlast interface to claim the maximum gas
+        * @dev Adds the claimed amount to the accumulated ETH yield
+        * @dev Emits a GasClaimed event with the claimed amount
+        */
     function claimMaxGas() public onlyOwner {
         address smartContract = address(this);
         uint256 claimed = BLAST.claimMaxGas(smartContract, smartContract);
@@ -114,6 +153,14 @@ contract BlastYieldAbstract is Ownable2StepUpgradeable {
         emit GasClaimed(claimed);
     }
 
+    /**
+        * @notice Withdraws accumulated ETH yield
+        * @dev This function can only be called by the contract owner
+        * @param recipient The address to receive the withdrawn ETH yield
+        * @param amount The amount of ETH yield to withdraw
+        * @custom:throws InsufficientETHYield if trying to withdraw more ETH than accumulated
+        * @custom:throws TransferFailed if the ETH transfer fails
+        */
     function withdrawETHYield(address payable recipient, uint256 amount) public onlyOwner {
         if (amount > accumulatedETHYield) {
             revert InsufficientETHYield(amount, accumulatedETHYield);
@@ -121,7 +168,7 @@ contract BlastYieldAbstract is Ownable2StepUpgradeable {
         accumulatedETHYield -= amount;
         (bool success, ) = recipient.call{value: amount}("");
         if (!success) {
-            revert ETHTransferFailed();
+            revert TransferFailed();
         }
     }
 
